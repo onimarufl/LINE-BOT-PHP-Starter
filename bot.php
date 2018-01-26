4d0803456ce7a8ed5c34af750bb15c78f6c5bc6d
onimarufl <31014216+onimarufl@users.noreply.github.com>
2018-01-26 12:42:56 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,10 +16,15 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
-			$replyToken = $event['replyToken'];
+			$replyToken = $event['replyToken']['text'];
 
 			// Build message to reply back
 			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+			
+			$replyToken = [
 				'type' => 'text',
 				'text' => $text
 			];
@@ -27,7 +32,7 @@ if (!is_null($events['events'])) {
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
-				'replyToken' => $replyToken,
+				'replyToken' => [$replyToken],
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);

a9289a443ea1e5587e6490abd143ed2005d73ba6
onimarufl <31014216+onimarufl@users.noreply.github.com>
2018-01-28 20:25:36 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,19 +1,20 @@
 <?php
+include "connect.php";
 $strAccessToken = '9B9ffZ7XJ/iWMWgJuqRV/oaVVMfELLEMmBjoIhqG9E5xdFvOHDNpiZBDjdi2deqYm4SdFCezBQjddXs1EgjLXmCJgBorihv3bfwUxW8zMCoT9EqBEs5CW6wnsUqEoJcKTGPyYznGlnG293DicIlZowdB04t89/1O/w1cDnyilFU=';
 
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
 
-if ($arrJson = "") {
-  echo "No Token";
-}else{
+
 
 $strUrl = "https://api.line.me/v2/bot/message/reply";
 
 $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
-
+if ($arrJson = "") {
+  echo "No Token";
+}else
 if($arrJson['events'][0]['message']['text'] == "ID"){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
@@ -52,5 +53,5 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
-}
+
 ?>

1d0422bd682ff4b6eafb3a3a9bd44470bcbae2cc
onimarufl <31014216+onimarufl@users.noreply.github.com>
2018-01-30 22:08:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -43,13 +43,7 @@ if($arrJson == ""){
 			$_SESSION["token"] = $row["token"];
 			$_SESSION["username"] = $row["username"];
 
-				if($arrJson['events'][0]['message']['text'] == "ID"){
-			  $arrPostData = array();
-			  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-			  $arrPostData['messages'][0]['type'] = "text";
-			  $arrPostData['messages'][0]['text'] = "สวัสดี ID คุณคือ ".$arrJson['events'][0]['source']['userId'];
-					
-			}else if($arrJson['events'][0]['message']['text'] == "รถ"){
+				 if($arrJson['events'][0]['message']['text'] == "รถ"){
 
 				$objDB = mysqli_select_db($objConnect,"sql12218252");
 				$s1 = "SELECT * FROM cars Where token = '$check'";
@@ -66,54 +60,7 @@ if($arrJson == ""){
 			  $arrPostData['messages'][0]['text'] = "รถของท่าน ".$_SESSION["Cartype"]."ทะเบียน ".$_SESSION["License"];
 						
 			
-			}else if($arrJson['events'][0]['message']['text'] == "พิกัด"){
-				
-				$objDB = mysqli_select_db($objConnect,"sql12218252");
-				
-				$s1 = "SELECT car.license, car.cartype,livedata.latitude,livedata.longitude 
-				FROM car INNER JOIN livedata ON car.token = livedata.token and car.token = '$check' ";
-				
-					$sql1 = mysqli_query($objConnect,$s1);
-					
-				
-
-				$row = mysqli_fetch_array($sql1);
-
-				$_SESSION["Cartype"] = $row["cartype"];
-				$_SESSION["License"] = $row["license"];
-				$_SESSION["Latitude"] = $row["latitude"];
-				$_SESSION["Longitude"] = $row["longitude"];
-					
-					 
-
-			  $arrPostData = array();
-			  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-			  
-			$arrPostData['messages'][0]['type'] = "location";
-			$arrPostData['messages'][0]['title'] = "".$_SESSION["Cartype"];
-			$arrPostData['messages'][0]['address'] = "".$_SESSION["License"];
-			$arrPostData['messages'][0]['latitude'] = $_SESSION["Latitude"];
-			$arrPostData['messages'][0]['longitude'] = $_SESSION["Longitude"];
-
-		
-			
-		}else if($arrJson['events'][0]['message']['text'] == "ที่ตั้ง"){
-
-			  $arrPostData = array();
-			  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-					
-			$latitude = '13.716013';
-			$longitude = '100.656906';
-			  
-			$arrPostData['messages'][0]['type'] = "location";
-			$arrPostData['messages'][0]['title'] = "บริษัท อีสท์อินโนเวชั่น จำกัด";
-			$arrPostData['messages'][0]['address'] = "ซอย อ่อนนุช 74/1 แยก 1 แขวง ประเวศ เขต ประเวศ กรุงเทพมหานคร 10250 โทรศัพท์: 02-052-4466";
-			$arrPostData['messages'][0]['latitude'] = $latitude ;
-			$arrPostData['messages'][0]['longitude'] = $longitude ;
-
-
-			
-		}
+			}
 		}else {
 
 			$arrPostData = array();

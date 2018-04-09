4184d87f9f8b09a4dfebc072047825e7406c33ce
onimarufl <31014216+onimarufl@users.noreply.github.com>
2018-04-09 20:27:13 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -32,7 +32,7 @@ $msg = $arrJson['events'][0]['message']['text'];
 	$s = "SELECT * FROM msgdata Where c_data = '$msg'";
 	$sql = mysqli_query($objConnect,$s);
 
-if(mysqli_num_rows($sql)==1){
+if(mysqli_num_rows($sql)>=1){
 			
 			$objDB = mysqli_select_db($objConnect,"sql12231545");
 			$s1 = "SELECT * FROM msgdata Where c_data = '$msg'";

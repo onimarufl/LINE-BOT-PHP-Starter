89bb71aa8849e0fbb82b8d8e8c70b6f5b47b74e9
onimarufl <31014216+onimarufl@users.noreply.github.com>
2018-04-21 12:50:03 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -2,8 +2,9 @@
 session_start(); //เปิด seesion เพื่อทำงาน
 echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
 
+include 'https://my-idol-3.000webhostapp.com/connect.php';
 //connectdb
-$host = "cs.nan.rmutl.ac.th/phpMi/index.php";
+/*$host = "cs.nan.rmutl.ac.th/phpMi/index.php";
 $username = "teerawat_agribot";
 $password = "jaidee#123!";
 $objConnect = mysqli_connect($host,$username,$password);
@@ -17,7 +18,7 @@ else
 {
 	echo "MySQL Connect Failed : Error : ".mysqli_error();
 }
-
+*/
 
 //Line Token
 $strAccessToken = '1OFasil/2dmg4zfIvklnFzY23slCclWjIgKyIwHnQcbg7ztGPVMZny6479Vnyeh8gCNpL9KJl5I6YfMpmNveUjbwcoi4f943KMjpHwmxb+pXKetgldM4DK2CUVZhRCvCoQYEAS5+yPkDLjwLQvm3RgdB04t89/1O/w1cDnyilFU=';

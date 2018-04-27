b9dcd8923e22a69a9123b6e3fabc7804642bd3ce
onimarufl <31014216+onimarufl@users.noreply.github.com>
2018-04-28 01:33:01 +0700

    Update bot2.php

--- bot2.php
+++ bot2.php
@@ -1,5 +1,5 @@
 <?php
-echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
+//echo '<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />';
 //Line Token
 $strAccessToken = '9B9ffZ7XJ/iWMWgJuqRV/oaVVMfELLEMmBjoIhqG9E5xdFvOHDNpiZBDjdi2deqYm4SdFCezBQjddXs1EgjLXmCJgBorihv3bfwUxW8zMCoT9EqBEs5CW6wnsUqEoJcKTGPyYznGlnG293DicIlZowdB04t89/1O/w1cDnyilFU=';
 $content = file_get_contents('php://input');
